503dd3b0b9fbcbdb4411aa1b207c97c12932cd25
yliya golovina <y.golovina@tg.dunice.net>
2024-01-23 15:50:02 +0300

    Added form for adding tasks

---
+++ src/components/AddTaskForm.vue
@@ -0,0 +1,59 @@
+<template>
+    <InputGroup class="task-form">
+        <InputText class="task-form__input" :placeholder="placeholder" />
+        <Button class="task-form__button" icon="pi pi-plus"/>
+    </InputGroup>
+</template>
+
+<script setup lang="ts">
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import InputGroup from 'primevue/inputgroup';
+
+defineProps<{
+    placeholder?: string,
+}>();
+</script>
+
+<style lang="scss" scoped>
+.task-form {
+    display: flex;
+    width: 500px;
+    margin-bottom: 40px;
+    border: var(--default-border);
+    border-radius: var(--default-border-radius);
+
+    &__button {
+        border: none;
+        background-color: var(--color-grey);
+        display: flex;
+        align-items: center;
+        border-left: var(--default-border);
+        border-radius: var(--default-border-radius);
+
+        &-icon {
+            width: 20px;
+            height: 20px;
+        }
+        &:focus {
+            box-shadow: none; 
+        }
+        &:hover {
+            background-color: var(--color-grey-dark)
+        }
+    }
+    
+    &__input {
+        padding: var(--padding-10);
+        width: 100%;
+        border-radius: var(--default-border-radius);
+        &::placeholder {
+            padding: 5px;
+        }
+        &:focus {
+            box-shadow: none;
+            
+        }
+    }
+}
+</style>
