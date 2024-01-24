af57616c8c533f04e2beb66028a85a282e5a859d
yliya golovina <y.golovina@tg.dunice.net>
2024-01-24 17:40:16 +0300

    Added button component for moving tasks to done list

---
+++ src/components/ButtonMoveToDone.vue
@@ -0,0 +1,27 @@
+<template>
+    <Button
+        class="button-move-to-done"
+        icon="pi pi-arrow-right"
+        @click="moveToDone"
+    />
+</template>
+  
+<script setup lang="ts">
+import { useTasksStore } from '@/stores/tasks';
+import Button from 'primevue/button';
+
+const { moveToDone } = useTasksStore();
+</script>
+  
+<style lang="scss" scoped>
+.button-move-to-done {
+    padding: 20px;
+    height: 50px;
+    align-self: center;
+    background-color: var(--color-grey);
+    &:hover {
+        background-color: var(--color-grey-dark);
+    }
+}
+
+</style>
